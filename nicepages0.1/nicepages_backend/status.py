c815efb0598a2c96c2a9abcca99077d5ed928fb0
Akash <akash.mittal@bonamisoftware.com>
2022-10-12 17:18:41 +0530

    sisu

--- nicepages0.1/nicepages_backend/status.py
+++ nicepages0.1/nicepages_backend/status.py
@@ -1,21 +1,7 @@
 import json
-# from db_configuration.config import *
-def nowOnline(uid):
-    # cnx = mysql.connector.connect(**config)
-    # cursor = cnx.cursor()
-    # query1 = f"SELECT uname from users where UID = {uid};"
-    # cursor.execute(query1)
-    # uname = cursor.fetchall()
-    # uname = uname[0][0]
-    # # data =  {f"{uid}":f"{uname}"}
-    # data={f"{uid}":{
-    #     "uname":f"{uname}"
-    #     }
-    # }
+def nowOnline(uid):  
     uid=str(uid)
     data={"uid":uid}
-    print(data)
     data=json.dumps(data,indent=4)
-    # print(data[0])
     with open('credentials.json','w+') as online:
         online.write(data)
